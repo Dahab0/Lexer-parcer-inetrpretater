985271b518cc46a876b427341eb6f158f5107507
Dahab Tahir Issakha <dahabtahirissakha@gmail.com>
2022-05-25 08:10:54 +0300

    ok

---
+++ src/com/company/UnOpNode.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class UnOpNode extends Node {
+    Token operator;
+    Node value;
+
+    public UnOpNode(Token operator, Node value) {
+        this.operator = operator;
+        this.value = value;
+    }
+
+}
